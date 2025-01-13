26755bbfd5637acce73e375402c8dd2f19980775
Mickiyas <Mickiyasbirhanie@gmail.com>
2025-01-13 11:10:09 +0000

    add href as example

--- src/component/Menu/Menu.jsx
+++ src/component/Menu/Menu.jsx
@@ -1,39 +1,24 @@
+import React, { Component } from "react";
+import menu from "../../assets/Data/data.js";
 
-import React, {Component} from 'react'
-import menu from '../../assets/Data/data.js'
+class Menu extends Component {
+  render() {
+    let { title, category, price, img, desc } = this.props;
+    return (
+      <div className="single-food">
+        <div className="img">
+          <a href="https://axum-restaurant.be/#menu">
+            <img src={img} />
+          </a>
+        </div>
+        <div className="title-price">
+          <h3>{title}</h3>
+          <p>{price}</p>
+        </div>
+        <div className="food-desc">{desc}</div>
+      </div>
+    );
+  }
+}
 
-    class  Menu extends Component {
-        render(){
-
-            let {title, category, price, img, desc} = this.props;
-        return(
-
-                
-                
-
-                <div className="single-food">
-            <div className="img">
-                <img
-                src={img}
-                />
-            </div>
-            <div className="title-price">
-                <h3>{title}</h3>
-                <p>{price}</p>
-            </div>
-            <div className="food-desc">
-                {desc}
-            </div>
-            </div> 
-        
-        
-
-            
-
-        )}
-
-        }
-        
-
-    export default Menu
-
+export default Menu;
